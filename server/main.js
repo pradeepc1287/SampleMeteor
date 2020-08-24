3260a509baab4d729be638b489ded815b1283b0e
pradeep cendra <prade@SDS-IT-030.cin2.creditinfonet.com>
2020-08-24 11:52:49 +0530

    port change

--- server/main.js
+++ server/main.js
@@ -1,5 +1,11 @@
 import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
+  app.set( 'port', ( process.env.PORT || 5000 ));
+
+// Start node server
+app.listen( app.get( 'port' ), function() {
+  console.log( 'Node server is running on port ' + app.get( 'port' ));
+  });
   // code to run on server at startup
 });
